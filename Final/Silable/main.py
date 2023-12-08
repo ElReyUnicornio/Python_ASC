23b17470eb461c200ec7f68a54e5d45ebceafbd6
ElReyUnicornio <vampirex.415@gmail.com>
2023-12-08 14:11:13 -0700

    refactor

--- Final/Silable/main.py
+++ Final/Silable/main.py
@@ -10,7 +10,7 @@ import silables as sb
 import accents as acns
 import os
 
-debug = False
+debug = True
 console = Console()
 
 MARKDOWN = """
@@ -44,6 +44,7 @@ def main():
     tonics_slbs = acns.Get_tonics(silables, tonics)
     types = acns.Get_clasification(tonics)
     console.print()
+    console.rule()
 
     #procesa y formatea la salida de datos--------------------------------------
 
@@ -61,7 +62,7 @@ def main():
 
     # Agrega columnas a la tabla
     table.add_column("Palabra", style="bold", justify="center")
-    table.add_column("Silaba", style="bold", justify="center")
+    table.add_column("Silaba", style="bold", justify="center", overflow="fold")
     table.add_column("Tonica", style="bold", justify="center")
     table.add_column("Tipo", style="bold", justify="center")
 
@@ -69,8 +70,12 @@ def main():
     for i, x in enumerate(separated_words):
         table.add_row("[bold red]" + x, "[bold yellow]" + silables[i].replace(" ", "-"), "[bold green]" + tonics_slbs[i], "[bold purple]" + types[i])
 
+    table.overflow = "fold"
     # Renderiza la tabla en la consola
     console.print(table, justify="center")
+    console.print()
+    
+    console.rule()
     console.print()
 
     if Confirm.ask("Desea probar de nuevo?"):
